ea1d5aed49f9bc5c20fe9f7b795a84f3e736fcb1
Gabriel Rosa <gabrieldsrosa@gmail.com>
2023-12-30 18:14:59 -0300

    feat: update folder structure

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,10 +1,10 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useContext, useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
+import AdoptedPetContext from "../context/AdoptedPetContext";
 import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
-import fetchPet from "./fetchPet";
+import fetchPet from "../fetchPet";
 import Carousel from "./Carousel";
 
 const Details = () => {
